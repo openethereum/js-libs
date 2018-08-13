d123aae8f6674dac4137e8b02247bdf8f01ff81b
Amaury Martiny <amaury.martiny@protonmail.com>
2018-08-13 11:05:58 +0200

    Fix isHex

--- packages/abi/src/util/types.ts
+++ packages/abi/src/util/types.ts
@@ -3,7 +3,24 @@
 //
 // SPDX-License-Identifier: MIT
 
-import * as isHexNpm from 'is-hex';
+const HEXDIGITS = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f'
+];
 
 export const isArray = (test: any): test is Array<any> => Array.isArray(test);
 
@@ -12,8 +29,24 @@ export const isError = (test: any): test is Error => test instanceof Error;
 export const isFunction = (test: any): test is Function =>
   typeof test === 'function';
 
-export const isHex = (test: any): boolean => isHexNpm(test);
+export const isHex = (test: any): boolean => {
+  if (!isString(test)) {
+    return false;
+  }
 
+  if (test.substr(0, 2) === '0x') {
+    return isHex(test.slice(2));
+  }
+
+  const lowerCaseTest = test.toLowerCase();
+  let hex = true;
+
+  for (let index = 0; hex && index < test.length; index++) {
+    hex = HEXDIGITS.includes(lowerCaseTest[index]);
+  }
+
+  return hex;
+};
 export const isObject = (test: any): test is object => typeof test === 'object';
 
 export const isString = (test: any): test is string => typeof test === 'string';
