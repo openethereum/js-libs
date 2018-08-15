6569eb5ec0e56cf10da425f4f23cad9159436a74
Amaury Martiny <amaury.martiny@protonmail.com>
2018-08-15 18:37:35 +0200

    Only show called rpc$

--- packages/light.js/src/overview/index.ts
+++ packages/light.js/src/overview/index.ts
@@ -35,7 +35,6 @@ if (typeof window !== 'undefined') {
   window.parity.rpcOverview = () => {
     const overview: Overview = {};
     Object.keys(rpc).forEach(key => {
-      console.log(key);
       // This one's more complex, we leave it for later
       if (key === 'makeContract') {
         return;
@@ -43,7 +42,21 @@ if (typeof window !== 'undefined') {
 
       const rpc$: RpcObservable<any, any> = (rpc as any)[key];
 
-      overview[key] = {};
+      // If the rpc$ has not been called, then we don't show it
+      if (!rpc$.metadata.calledWithArgs) {
+        return;
+      }
+
+      overview[key] = { calledWithArgs: {} };
+
+      // We populate the `calledWithArgs` in a human-readable way
+      Object.keys(rpc$.metadata.calledWithArgs).map(calledWithArgsKey => {
+        const subject$ = rpc$.metadata.calledWithArgs[calledWithArgsKey];
+        overview[key].calledWithArgs[calledWithArgsKey] = {
+          currentValue: subject$._events && subject$._events[0],
+          subscribersCount: subject$.observers.length
+        };
+      });
 
       // We add all calls of this RpcObservable
       if (rpc$.metadata.calls) {
@@ -55,38 +68,12 @@ if (typeof window !== 'undefined') {
         overview[key].dependsOn = rpc$.metadata.dependsOn.toString();
       }
 
-      // We make the `calledWithArgs` field human-readable
-      if (rpc$.metadata.calledWithArgs) {
-        overview[key].calledWithArgs = {};
-
-        Object.keys(rpc$.metadata.calledWithArgs).map(calledWithArgsKey => {
-          const subject$ = rpc$.metadata.calledWithArgs[calledWithArgsKey];
-          overview[key].calledWithArgs[calledWithArgsKey] = {
-            currentValue: subject$._events && subject$._events[0],
-            subscribersCount: subject$.observers.length
-          };
-        });
-      }
-
       // We add a human-readable version of `frequency` field
       if (rpc$.metadata.frequency) {
         overview[key].frequency = rpc$.metadata.frequency.map(
           frequency$ => frequency$.metadata.name
         );
       }
-
-      // We remove all the metadata keys that are null, empty or functions,
-      // for clarity while console.logging it.
-      // Object.keys(overview[key]).forEach(innerKey => {
-      //   if (
-      //     !overview[key][innerKey] ||
-      //     (Array.isArray(overview[key][innerKey]) &&
-      //       !overview[key][innerKey].length) ||
-      //     typeof overview[key][innerKey] === 'function'
-      //   ) {
-      //     delete overview[key][innerKey];
-      //   }
-      // });
     });
 
     return overview;
