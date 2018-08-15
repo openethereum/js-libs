c9dbf67f68d3fd35c3242e519ae292c9987d1a20
Amaury Martiny <amaury.martiny@protonmail.com>
2018-08-15 17:18:13 +0200

    Use bignumber for blocks

--- packages/light.js/src/frequency/blocks.ts
+++ packages/light.js/src/frequency/blocks.ts
@@ -3,32 +3,36 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { filter, map } from 'rxjs/operators';
+import BigNumber from 'bignumber.js';
+import { filter } from 'rxjs/operators';
 
 import api from '../api';
 import createOnFromPubsub from './utils/createOnFromPubsub';
 import { FrequencyObservable } from '../types';
 
+const ZERO = new BigNumber(0);
+
 /**
  * Observable that emits on every new block.
  */
-export const onEveryBlock$ = createOnFromPubsub('eth_blockNumber', api).pipe(
-  map(v => +v) // Return number instead of BigNumber
-) as FrequencyObservable<number>;
+export const onEveryBlock$ = createOnFromPubsub<BigNumber>(
+  'eth_blockNumber',
+  api
+);
 onEveryBlock$.metadata = { name: 'onEveryBlock$' };
 
 /**
  * Observable that emits on every 2nd block.
  */
 export const onEvery2Blocks$ = onEveryBlock$.pipe(
-  filter(n => n % 2 === 0) // Around ~30s on mainnet
-) as FrequencyObservable<number>;
+  filter(n => n.mod(2).isEqualTo(ZERO)) // Around ~30s on mainnet
+) as FrequencyObservable<BigNumber>;
 onEvery2Blocks$.metadata = { name: 'onEvery2Blocks$' };
 
 /**
  * Observable that emits on every 4th block.
  */
 export const onEvery4Blocks$ = onEveryBlock$.pipe(
-  filter(n => n % 4 === 0) // Around ~1min on mainnet
-) as FrequencyObservable<number>;
+  filter(n => n.mod(4).isEqualTo(ZERO)) // Around ~1min on mainnet
+) as FrequencyObservable<BigNumber>;
 onEvery4Blocks$.metadata = { name: 'onEvery4Blocks$' };
