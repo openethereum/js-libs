7df45311af618044c627220bda04f73e7773f604
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-11 17:52:03 +0200

    Use function instead of const

--- packages/light.js/src/rpc/parity/parity.ts
+++ packages/light.js/src/rpc/parity/parity.ts
@@ -10,25 +10,31 @@ import { switchMapPromise } from '../../utils/operators';
 /**
  * Get accounts info. Calls `parity_accountsInfo`.
  *
+ * @param api - The Api object used to create this {@link RpcObservable}.
+ * @param frequency - The FrequencyMap used to create this {@link RpcObservable}.
  * @return - An Observable containing all info that can be
  * accessed by user concerning accounts.
  */
-export const accountsInfo$ = (_: any, frequency: FrequencyMap) =>
-  createRpc$<AccountsInfo, AccountsInfo>({
+export function accountsInfo$(_: any, frequency: FrequencyMap) {
+  return createRpc$<AccountsInfo, AccountsInfo>({
     frequency: [frequency.onAccountsInfoChanged$],
     name: 'accountsInfo$'
   });
+}
 
 /**
  * Get the name of the current chain. Calls `parity_netChain`.
  *
+ * @param api - The Api object used to create this {@link RpcObservable}.
+ * @param frequency - The FrequencyMap used to create this {@link RpcObservable}.
  * @return - An Observable containing the name of the
  * current chain.
  */
-export const chainName$ = (api: any, frequency: FrequencyMap) =>
-  createRpc$<any, string>({
+export function chainName$(api: any, frequency: FrequencyMap) {
+  return createRpc$<any, string>({
     calls: ['parity_netChain'],
     frequency: [frequency.onStartup$],
     name: 'chainName$',
     pipes: () => [switchMapPromise(() => api.parity.netChain())]
   });
+}
