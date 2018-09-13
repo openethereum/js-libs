6e99b6749a0cba678e381ab39dc916d781e8704d
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-13 14:09:35 +0200

    Remove overview

--- packages/light.js/src/index.ts
+++ packages/light.js/src/index.ts
@@ -6,12 +6,20 @@
 import { setProvider } from './api';
 import frequency from './frequency';
 import rpc from './rpc';
-import './overview';
 
 export * from './utils/isLoading';
 export * from './types';
 export { withoutLoading } from './utils/operators/withoutLoading';
 
 export { frequency };
-export const { balanceOf$ } = rpc;
+export const {
+  accounts$,
+  accountsInfo$,
+  balanceOf$,
+  blockNumber$,
+  defaultAccount$,
+  myBalance$,
+  peerCount$,
+  syncStatus$
+} = rpc;
 export default { setProvider };
