743a0af9eab328bd175f1e054815e73a1198df4d
Amaury Martiny <amaury.martiny@protonmail.com>
2018-11-27 13:44:51 +0100

    Don't update git config

--- scripts/lerna-publish.sh
+++ scripts/lerna-publish.sh
@@ -4,8 +4,6 @@
 
 set -e # Quits if there's an error
 
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
 git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 
 lerna version patch --yes -m "[ci skip] Publish %s"
