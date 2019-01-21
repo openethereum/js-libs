d80eb903aea029e4385d83d4d261b92c66199cc6
Axel Chalon <xaxel@protonmail.com>
2019-01-21 22:43:03 +0100

    lerna: use `--conventional-commits`  to bump patch/minor/major & generate changelog

--- scripts/lerna-publish.sh
+++ scripts/lerna-publish.sh
@@ -6,5 +6,5 @@ set -e # Quits if there's an error
 
 git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 
-lerna version patch --yes -m "[ci skip] Publish %s"
+lerna version --conventional-commits --yes -m "[ci skip] Publish %s"
 lerna publish from-git --yes
